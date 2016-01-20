b9b69f4139906afe862b43ea7af18c40389e7c86
Chris Oakley <CAOakleyII@gmail.com>
2016-01-20 05:47:34 -0500

    * initial commit from private repo to public. * removed private api key from source and moved to untracked settings.json

--- server/spotify.js
+++ server/spotify.js
@@ -3,7 +3,7 @@ ServiceConfiguration.configurations.update(
   {
     $set: {
       "clientId": "ea56b81418334eb2b1387c34ee817e2d",
-      "secret": "edc77ac454e046158e08a11eeabb7c49"
+      "secret": Meteor.settings.spotifyKey
     }
   },
   { upsert: true }
